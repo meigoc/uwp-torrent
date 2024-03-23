146e6312a5fe3f68603aa7de27944157c6afa4c0
Meigo <73817505+meigoc@users.noreply.github.com>
2024-03-24 02:08:19 +0300

    🔥Update v1.1

--- src/meigo/forms/MainForm.php
+++ src/meigo/forms/MainForm.php
@@ -32,10 +32,13 @@ class MainForm extends AbstractForm
         
         $Menu = new UXMenu('Файл');
         $vid = new UXMenu('Вид');
+        $ser = new UXMenu('Сервис');
         $spr = new UXMenu('Справка');
+        
                
         $mainMenu->menus->add($Menu);
         $mainMenu->menus->add($vid);
+        $mainMenu->menus->add($ser); 
         $mainMenu->menus->add($spr); 
         $mainMenu->anchors = ['left' => 0, 'right' => 0];
         
@@ -45,11 +48,23 @@ class MainForm extends AbstractForm
         $open->image = new UXImage('res://.data/img/icons/plus16.png');    
          
             
-        $menuItem1 = new UXMenuItem('Добавить торрент-файл...', $open);
+        $aff = new UXMenuItem('Добавить торрент-файл...', $open);
         
-        $menuItem1->on('action', function ($e) use ($menuItem1){
+        $aff->on('action', function ($e) use ($aff){
        
            $ag = $this->fileChooser->execute();
+           
+           $file = TorrentFile::of($ag);
+           $name = fs::name($ag);
+           $this->table->items->add(['name' => $name,'size' => "N/AБайт",'status' => "Загрузка",'seeds' => "0 (0)",'peers' => "0 (0)"]); 
+           
+           $this->textArea->text .= "CreatedBy: ".$file->createdBy."\n";
+           $this->textArea->text .= "CreationDate: ".$file->creationDate."\n";
+           $this->textArea->text .= "Encoding: ".$file->encoding."\n";
+           $this->textArea->text .= "SaveAs: ".$file->saveAs."\n";
+           $this->textArea->text .= "Comment: ".$file->comment."\n";
+           $this->textArea->text .= "AnnouceUrl: ".$file->announceURL."\n";
+           
            $dm = new DownloadManager(TorrentFile::of($ag));
            $dm->startDownload(function () {
                // callback running every 1 second if download doesn't complete
@@ -64,7 +79,7 @@ class MainForm extends AbstractForm
           
         });                 
       
-        $Menu->items->add($menuItem1); 
+        $Menu->items->add($aff); 
                
 
  
@@ -82,6 +97,35 @@ class MainForm extends AbstractForm
         });                         
       
         $Menu->items->add($menuItem3);
+        
+        
+        
+        $create = new UXImageView();
+      
+        $create->image = new UXImage('res://.data/img/icons/bundleMake32.png');    
+           
+                
+        $menuItem4 = new UXMenuItem('Создать торрент', $create); 
+        
+        $menuItem4->on('action', function ($e) use ($menuItem4){
+       
+       $o = $this->dirChooser->execute();
+       $p = $this->fileChooserAlt->execute();
+       
+           $tp = TorrentProcessor();
+        $tp->setCreationDate(1710622499);
+        $tp->setCreator("jphp torrent api");
+        $tp->setEncoding("UTF-8");
+$tp->setComment("test");
+$tp->setPieceLength(100);
+$tp->setAnnounceURL(null); // Often, you can leave empty, because the tracker itself overwrites this header.
+$tp->setName("test"); // In what folder will be the data downloaded through our torrent file
+$tp->addFile($p);
+$tp->save("./test.torrent");
+          
+        });                         
+      
+        $ser->items->add($menuItem4);
     }
 
 
@@ -122,6 +166,7 @@ class MainForm extends AbstractForm
     {    
         app()->shutdown();
     }
+
 
 
 }
